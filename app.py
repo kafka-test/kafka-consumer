5f7b8c1d69a590b78317f93ff9e3d4d204f3e21e
hippyod <hippyod@yahoo.com>
2022-09-12 13:56:11 -0500

    testing

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ kafka_server = os.environ['KAFKA_SERVER']
 kafka_group = os.environ['KAFKA_GROUP']
 kafka_topic = os.environ['KAFKA_TOPIC']
 
+#  ssl_cafile = '/mnt/kafka-config/ca.crt',
 consumer = KafkaConsumer(kafka_topic,
                          group_id=kafka_group,
                          bootstrap_servers = kafka_server,
-                         ssl_cafile = '/mnt/kafka-config/ca.crt',
+                         ssl_cafile = '/vault/secrets/ca.crt',
                          security_protocol='SSL',
                          consumer_timeout_ms = 10000,
                          enable_auto_commit=True)
